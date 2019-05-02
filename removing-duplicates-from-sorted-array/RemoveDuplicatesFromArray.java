98632df0351296a052533ca353becb0df4cfa9ea
Pranav Kapoor <pranavkapoorr@gmail.com>
2019-05-01 22:49:46 -0400

    removing duplicates

---
+++ removing-duplicates-from-sorted-array/RemoveDuplicatesFromArray.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils.Collections;
+
+public class RemoveDuplicatesFromArray{
+
+    private int[] removeDuplicates(int[] array){
+        return Arrays.stream(array).distinct().toArray();
+    }
+
+    public static void main(String[] args){ 
+        int[] myArray = {1,2,2,3,3,4,5,6};
+        System.out.println( Arrays.toString(new RemoveDuplicatesFromArray().removeDuplicates(myArray)));
+    }
+}
