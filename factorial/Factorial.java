9f3172bbdc2e5d71bf541dc4323accb68cc77fcf
Pranav Kapoor <pranavkapoorr@gmail.com>
2019-05-03 13:00:20 -0400

    factorial

---
+++ factorial/Factorial.java
@@ -0,0 +1,10 @@
+public class Factorial{
+
+    private int findFactorial(int number){
+        return number == 0||number == 1? 1 : number * findFactorial(number - 1);
+    }
+
+    public static void main(String[] args){
+        System.out.println(new Factorial().findFactorial(5));
+    }
+}
