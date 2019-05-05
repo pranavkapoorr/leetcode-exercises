57c53b0da6f63c3c6e764f3d2bdea04f59432414
Pranav Kapoor <pranavkapoorr@gmail.com>
2019-05-04 23:58:26 -0400

    firstuniquechar

---
+++ find-first-unique-char-index-in-string/UniqueCharIndex.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class UniqueCharIndex{
+
+    private int findIndex(String inputWord){
+        String word = inputWord.toLowerCase();
+        System.out.println("word: "+word);
+        int index = -1;
+        if(word.length()>0 && !word.equals(null) && !word.equals("")){
+            int[] ch = new int[26];
+            for(int i = 0;i<word.length();i++){
+                ch[word.charAt(i) - 'a']++;
+            }
+            
+            for(int i = 0;i<ch.length;i++){
+                if(ch[i]==1){
+                    index = i;
+                    break;
+                }
+            }
+        }
+        return index;
+    }
+    public static void main(String[] args){
+        int result = new UniqueCharIndex().findIndex("java");
+        if(result == -1){
+            System.out.println("No unique character!");
+        }else{
+            System.out.println("Unique character found at index: "+result);
+        }
+    }
+}
