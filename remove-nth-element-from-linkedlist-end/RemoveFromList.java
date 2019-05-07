8d4bd7d8e4f2344fef4d09479ded7ffcd50f22d1
Pranav Kapoor <pranavkapoorr@gmail.com>
2019-05-06 23:27:43 -0400

    UniqueRobotPaths

---
+++ remove-nth-element-from-linkedlist-end/RemoveFromList.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class RemoveFromList{
+
+    private LinkedList removeFromEnd(LinkedList list, int index){
+        list.remove(list.size()-index);
+        return list;
+    }
+
+    public static void main(String[] args){
+        LinkedList list = new LinkedList<>();
+        list.addAll(Arrays.asList(1,2,3,4,5));
+        System.out.println(new RemoveFromList().removeFromEnd(list,2));
+    }
+}
