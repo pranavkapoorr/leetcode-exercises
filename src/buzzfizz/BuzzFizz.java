a2be39fb342b8892786cc477faa2711369910751
Pranav Kapoor <pranavkapoorr@gmail.com>
2019-09-18 12:32:48 -0400

    logicchanged

--- src/buzzfizz/BuzzFizz.java
+++ src/buzzfizz/BuzzFizz.java
@@ -11,12 +11,12 @@ But for multiples of three it should output “Fizz” instead of the number and
     private ArrayList<Object> createMagic(int n){
         ArrayList<Object> tempList = new ArrayList<>();
         for(int i = 1; i <= n; i ++){
-                if(i % 3 == 0){
-                    tempList.add("Fizz");
-                }else if(i % 5 == 0){
+	        	if((i % 3 == 0 && i % 5 == 0)){
+	                tempList.add("BuzzFizz");
+	            }else if(i % 5 == 0){
                     tempList.add("Buzz");
-                }else if((i % 3 == 0 && i % 5 == 0)){
-                    tempList.add("BuzzFizz");
+                }else if(i % 3 == 0){
+                    tempList.add("Fizz");
                 }else{
                     tempList.add(String.valueOf(i));
                 } 
@@ -26,6 +26,6 @@ But for multiples of three it should output “Fizz” instead of the number and
 
 
     public static void main(String[] args) {
-        System.out.println(new BuzzFizz().createMagic(11));
+        System.out.println(new BuzzFizz().createMagic(15));
     }
 }
