b7b4cd4badf7b18f4db8498abed4245566a8787a
Pranav Kapoor <pranavkapoorr@gmail.com>
2019-09-26 22:19:57 -0400

    lil fix

--- src/findingsmallestmissinginteger/Solution.java
+++ src/findingsmallestmissinginteger/Solution.java
@@ -1,6 +1,7 @@
 package findingsmallestmissinginteger;
 
 import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Solution {
     public int solution(int[] A) {
@@ -20,8 +21,24 @@ public class Solution {
         }
     	return 1;
     }
+    public int trialSolution(int[] A) {
+    	AtomicInteger min = new AtomicInteger(0);
+    	if(A.length > 0) {
+    		min.getAndIncrement();
+    		Arrays.stream(A)
+    				.sorted()
+    				.distinct()
+    				.forEach(e -> {
+    					if(e > 0 && e==min.get()) {
+    						min.incrementAndGet();
+    					}
+    				});
+    	}
+    	
+    	return min.get();
+    }
     public static void main(String[] args) {
     	int[] A = {0,1,2,3,4,5,7,8,9};
-		System.out.println(new Solution().solution(A));
+		System.out.println(new Solution().trialSolution(A));
 	}
 }
